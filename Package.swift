db90e378840feee6ec9902b02294508d3ec42296
Saleh Albuga <samalbuga@outlook.com>
2020-06-05 04:54:08 +0300

    Package name correction

--- Package.swift
+++ Package.swift
@@ -4,13 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    //name: "AzureFunctions",
-    name: "AzureFunctions",
+    name: "azure-functions-swift",
     platforms: [
         .macOS(.v10_15)
     ],
     products: [
-  //      .executable(name: "functions", targets: ["AzureFunctions"]),
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
            .library(
                name: "AzureFunctions",
