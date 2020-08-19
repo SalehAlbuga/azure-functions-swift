dea092f47331f7c64578e6ae444a1e7304f73780
Saleh Albuga <samalbuga@outlook.com>
2020-08-19 19:52:35 +0300

    Update framework version in examples

--- Examples/Classic/timer/Package.swift
+++ Examples/Classic/timer/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SalehAlbuga/azure-functions-swift", from: "0.6.1"),
+        .package(url: "https://github.com/SalehAlbuga/azure-functions-swift", from: "0.6.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
